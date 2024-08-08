5d32bb03881beb2881699a1f3968a0e15cf99d5f
Igor Panarin <59224857+networksuperman@users.noreply.github.com>
2024-08-08 13:16:43 +0300

    Update variables.tf

--- terraform/terraform/variables.tf
+++ terraform/terraform/variables.tf
@@ -8,21 +8,6 @@ variable "host_ip" {
   default = "0.0.0.0/0"
 }
 
-variable "token" {
-  type        = string
-  description = "OAuth-token; https://cloud.yandex.ru/docs/iam/concepts/authorization/oauth-token"
-}
-
-variable "cloud_id" {
-  type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
-}
-
-variable "folder_id" {
-  type        = string
-  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
-}
-
 variable "default_zone" {
   default     = "ru-central1-a"
 }
